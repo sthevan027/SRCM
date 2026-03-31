d78a6c20c0a290e97c602ff2cfbffd40ca5f5e0d
sthevan027 <sthevan.ssantos@gmail.com>
2026-03-31 13:36:04 -0300

    update Index.TSX

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
     <main>
-      <h1>SRCM</h1>
+      <h1>SRCM-JL</h1>
       <p>Sistema de Rastreabilidade e Controle de Materiais</p>
       <p>Bem-vindo ao SRCM. Escaneie um QR Code para consultar materiais.</p>
+      <p>Desenvolvedor: Sthevan Santos</p>
+      <p>Arquiteto: Victor</p>
     </main>
   );
 }
